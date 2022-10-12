69032ff691ce9134adb36b8ae0db4ea7be8d3c44
Inderpal Singh <92572473+99inder@users.noreply.github.com>
2022-10-12 21:17:07 +0530

    Create Item Designing Initiated

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <AnimatePresence mode="wait">
             <div className="w-screen h-auto flex flex-col bg-primary">
                 <Header />
-                <main className='w-full mt-16 px-4 py-6 md:mt-24 md:px-20 md:py-10 '>
+                <main className='w-full mt-16 px-4 py-6 md:px-20 md:mt-24 md:py-10'>
                     <Routes>
                         <Route path="/*" element={<MainContainer />} />
                         <Route path="/createItem" element={<CreateContainer />} />
--- src/utils/heroData.js
+++ src/utils/heroData.js
@@ -32,4 +32,4 @@ export const heroData= [
         desc: "Chilled Coca-Cola",
         price: 49,
     },
-]
+];
